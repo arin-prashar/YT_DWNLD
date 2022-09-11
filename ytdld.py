2e5a34de56edeb91fc0dd32c4435b461b2c7a512
Arin Prashar <112185917+DKGamer02AI@users.noreply.github.com>
2022-09-11 16:47:02 +0530

    upload

---
+++ ytdld.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+
+yt=YouTube(link)
+print("Title- ",yt.title)
+
+yd = yt.streams.get_highest_resolution()
+yd.download("/yt downloads")
